72cf0e5e33f71bd1d0c90f1bac78c1d58d59cd2d
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2025-04-23 10:43:16 +0530

    Add files via upload

---
+++ admin/files/footer.php
@@ -0,0 +1,11 @@
+  <footer class="footer">
+    <div class="container-fluid">
+      <div class="copyright">
+        &copy;
+        <script>
+          document.write(new Date().getFullYear())
+        </script>, Powered By
+        <a href="#" target="_blank">Durgesh4993</a>.
+      </div>
+    </div>
+  </footer>
